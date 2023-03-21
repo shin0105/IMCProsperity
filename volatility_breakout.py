caa91bab61bc6a2006c8e07dbf38b3a67798c485
procyon-yim <69961142+procyon-yim@users.noreply.github.com>
2023-03-22 01:04:23 +0900

    demo ver3.

--- volatility_breakout.py
+++ volatility_breakout.py
@@ -21,17 +21,18 @@ class Trader:
                 
                 seq = state.timestamp/100  # 1, 2, 3, 4, ... 
                 current_day = seq // one_day
+                orders: list[Order] = []
+                order_depth: OrderDepth = state.order_depths[product]
+                best_ask = min(order_depth.buy_orders.keys())
+                best_ask_volume = order_depth.buy_orders[best_ask]
+                best_bid = max(order_depth.sell_orders.keys())
+                best_bid_volume = order_depth.sell_orders[best_bid]
+                mid_price = (best_ask + best_bid) / 2
 
                 if seq % one_day == 0:  # at the start of the day 
-                    bid_prices = state.order_depths[product].buy_orders.keys()
-                    ask_prices = state.order_depths[product].sell_orders.keys()
-                    mid_price = (bid_prices[0] + ask_prices[0])/2
                     self.daily_prices['start of %d' % current_day] = mid_price
                     
                 elif seq % one_day == one_day - 1:  # at the end of the day
-                    bid_prices = state.order_depths[product].buy_orders.keys()
-                    ask_prices = state.order_depths[product].sell_orders.keys()
-                    mid_price = (bid_prices[0] + ask_prices[0])/2
                     self.daily_prices['end of %d' % current_day] = mid_price
 
                 if current_day == 0:
@@ -40,25 +41,13 @@ class Trader:
                 else:
                     
                      delta = self.daily_prices['end of %d' % current_day-1] - self.daily_prices['start of %d' % current_day-1]                    
-                     bid_prices = state.order_depths[product].buy_orders.keys()
-                     ask_prices = state.order_depths[product].sell_orders.keys()
-                     mid_price = (bid_prices[0] + ask_prices[0])/2
-
                      buy_condition = mid_price > self.daily_prices['start of %d' % current_day] + k * delta
 
                      if buy_condition:
-                        orders: list[Order] = []
-                        order_depth: OrderDepth = state.order_depths[product]
-                        best_ask = min(order_depth.buy_orders.keys())
-                        best_ask_volume = order_depth.buy_orders[best_ask]
                         orders.append(Order(product, best_ask, -best_ask_volume))
                         print("BUY", str(-best_ask_volume) + "x", best_ask)
                     
                      if seq % one_day == one_day - 1:
-                        orders: list[Order] = []
-                        order_depth: OrderDepth = state.order_depths[product]
-                        best_bid = max(order_depth.sell_orders.keys())
-                        best_bid_volume = order_depth.sell_orders[best_bid]
                         orders.append(Order(product, best_bid, -best_bid_volume))
                         print("BUY", str(-best_bid_volume) + "x", best_bid)
                         
