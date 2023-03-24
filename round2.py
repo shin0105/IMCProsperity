c52082ea7cd1b51bb758992cf809738602e26b5b
vxbrandon <vxbrandon@gmail.com>
2023-03-24 02:04:49 +0100

    Final round2 algorithms

--- round2.py
+++ round2.py
@@ -3,11 +3,26 @@ from datamodel import OrderDepth, TradingState, Order
 import numpy as np
 import math
 
+TP_list = []
+SMAshort_list = [0]
+SMAlong_list = [0]
+EMAshort_list = [0]
+EMAlong_list = [0]
+macd_list = []
 
-"""
-Utils
-"""
+TP_list1 = []
+SMAshort_list1 = [0]
+SMAlong_list1 = [0]
+EMAshort_list1 = [0]
+EMAlong_list1 = [0]
+macd_list1 = []
 
+TP_list2 = []
+SMAshort_list2 = [0]
+SMAlong_list2 = [0]
+EMAshort_list2 = [0]
+EMAlong_list2 = [0]
+macd_list2 = []
 
 def get_mid_price(orders: dict, is_weighted: bool = False):
     mid_price = 0
@@ -35,7 +50,6 @@ PEARLS_POSITION_LIMITS = 19
 BANANAS_POSITION_LIMITS = 20
 COCONUT_POSITION_LIMITS = 600
 PINA_POSITION_LIMITS = 800
-
 
 """
 Product classes
@@ -89,15 +103,14 @@ class Banana:
                 return self.position
 
 
-
-
-
 # Initialize class
 pearl = Pearl()
 banana = Banana()
-TP_list = [0]
+
+prev_position = 0
 
 class Trader:
+
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
         Only method required. It takes all buy and sell orders for all symbols as an input,
@@ -106,74 +119,13 @@ class Trader:
         # Initialize the method output dict as an empty dict
         result = {}
         position = state.position
-        print(position)
+
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
-
+            print(position.keys())
+            print(position.values())
             # Check if the current product is the 'PEARLS' product, only then run the order logic
-            if product in ["COCONUTS", "PINA_COLADAS"]:
-                position_limit = COCONUT_POSITION_LIMITS if product is "COCONUTS" else PINA_POSITION_LIMITS
-
-                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
-                order_depth: OrderDepth = state.order_depths[product]
-
-                if (len(position) == 0) or (product not in position.keys()):
-                    current_position = 0
-                else:
-                    current_position = position[product]
-
-                # Initialize the list of Orders to be sent as an empty list
-                ordersB: List[Order] = []
-
-                TP = (
-                        float(
-                            min(order_depth.sell_orders.keys())
-                            + max(order_depth.buy_orders.keys())
-                        )
-                        / 2
-                )
-                TP_list.append(TP)
-                n = int(state.timestamp / 100)  # n denotes current timestep
-                m = 30
-                d = 2
-
-                if n > m:
-                    SMA = np.mean(TP_list[n - m - 1: n])
-                    UB = SMA + d * np.std(TP_list[n - m - 1: n]) / math.sqrt(m)
-                    MB = np.sum(TP_list[n - m - 1: n]) / m
-                    LB = SMA - d * np.std(TP_list[n - m - 1: n]) / math.sqrt(m)
-
-                else:
-                    SMA = TP_list[n - 1]
-                    UB = SMA + d * np.std(TP_list) / math.sqrt(m)
-                    MB = TP_list[n - 1]
-                    LB = SMA - d * np.std(TP_list) / math.sqrt(m)
-
-                best_bid = max(order_depth.buy_orders.keys())
-                best_bid_volume = order_depth.buy_orders[best_bid]
-
-                best_ask = min(order_depth.sell_orders.keys())
-                best_ask_volume = order_depth.sell_orders[best_ask]
-
-                max_buy_volume = abs(position_limit - current_position)
-                max_sell_volume = -abs(-position_limit - current_position)
-
-                if UB < TP:
-                    ordersB.append(Order(product, best_bid, max_sell_volume))
-
-                if LB > TP:
-                    ordersB.append(Order(product, best_ask, max_buy_volume))
-
-                result[product] = ordersB
-
-                print(
-                    f"Product: {product}/ Position: {current_position}\n"
-                    f"BUY at price {best_ask} with volume {max_buy_volume}\n"
-                    f"Sell at price {best_bid} with volume {max_sell_volume}\n"
-                )
-                print("")
-
-            elif product == "PEARLS":
+            if product == 'PEARLS':
                 ordersP: List[Order] = []
                 if (len(position) == 0) or (product not in position.keys()):
                     current_position = 0
@@ -186,7 +138,7 @@ class Trader:
                 best_ask = min(order_depth.sell_orders.keys())
                 best_bid = max(order_depth.buy_orders.keys())
                 if (len(order_depth.sell_orders) > 0) and (
-                    len(order_depth.buy_orders) > 0
+                        len(order_depth.buy_orders) > 0
                 ):
                     current_mid_price = 1 / 2 * (best_ask + best_bid)
                     pearl.update_avg_price(current_mid_price)
@@ -211,7 +163,8 @@ class Trader:
                         f"Sell at price {best_bid} with volume {max_sell_volume}\n"
                     )
                     print("")
-            elif product == "BANANAS":
+
+            elif product == 'BANANAS':
                 ordersB: List[Order] = []
 
                 if (len(position) == 0) or (product not in position.keys()):
@@ -248,51 +201,186 @@ class Trader:
                         f"Sell at price {best_bid} with volume {max_sell_volume}\n"
                     )
                 result[product] = ordersB
-            # elif product == "COCONUTS":
-            #
-            #     ordersC: List[Order] = []
-            #
-            #     order_depth: OrderDepth = state.order_depths[product]
-            #
-            #     best_ask = min(order_depth.sell_orders.keys())
-            #     best_ask_volume = -10
-            #
-            #     if len(position) == 0:
-            #         current_position = 0
-            #     else:
-            #         current_position = position[product]
-            #
-            #     # print(
-            #     #     f"{product} and {current_position} BUY at price {best_ask} with volume {best_ask_volume}"
-            #     # )
-            #     ordersC.append(Order(product, best_ask, -best_ask_volume))
-            #
-            #     result[product] = ordersC
-            #
-            # elif product == "PINA_COLADAS":
-            #
-            #     ordersPi: List[Order] = []
-            #
-            #     order_depth: OrderDepth = state.order_depths[product]
-            #
-            #     best_ask = min(order_depth.sell_orders.keys())
-            #     best_ask_volume = -10
-            #
-            #     if len(position) == 0:
-            #         current_position = 0
-            #     else:
-            #         current_position = position[product]
-            #
-            #     # print(
-            #     #     f"{product} and {current_position} BUY at price {best_ask} with volume {best_ask_volume}"
-            #     # )
-            #     ordersPi.append(Order(product, best_ask, -best_ask_volume))
-            #
-            #     result[product] = ordersPi
+
+            elif product == 'COCONUTS':
+
+                ordersC: list[Order] = []
+
+                order_depthC: OrderDepth = state.order_depths[product]
+
+                if (len(position) == 0) or (product not in position.keys()):
+                    current_position = 0
+                else:
+                    current_position = position[product]
+
+                print(current_position)
+
+                n1 = 12
+                n2 = 26
+                n3 = 9
+
+                m = int(state.timestamp / 100)
+
+                TP = float(min(order_depthC.sell_orders.keys()) + max(order_depthC.buy_orders.keys())) / 2
+                TP_list.append(TP)
+
+                if m > n2:
+                    SMAlong = np.mean(TP_list[m - n2 - 1:m])
+
+                else:
+                    SMAlong = TP_list[m - 1]
+                SMAlong_list.append(SMAlong)
+
+                print(SMAlong)
+
+                if m > n1:
+                    SMAshort = np.mean(TP_list[m - n1 - 1:m])
+
+                else:
+                    SMAshort = TP_list[m - 1]
+                SMAshort_list.append(SMAshort)
+
+                print(SMAshort)
+
+                k2 = 2 / (n2 + 1)
+                k1 = 2 / (n1 + 1)
+
+                EMAlong = k2 * SMAlong + (1 - k2) * EMAlong_list[m - 1]
+                EMAlong_list.append(EMAlong)
+                print(EMAlong)
+
+                EMAshort = k1 * SMAshort + (1 - k1) * EMAshort_list[m - 1]
+                EMAshort_list.append(EMAshort)
+                print(EMAshort)
+
+                macd = EMAshort - EMAlong
+                macd_list.append(macd)
+
+                if m > n3:
+                    macd_signal = np.mean(macd_list[m - n3 - 1:m])
+                else:
+                    macd_signal = macd_list[m - 1]
+
+                print(macd - macd_signal)
+
+                if macd - macd_signal > 0:
+                    if current_position < -500:
+                        LOT_SIZE = int((600 - current_position) / 12)
+                        best_ask = min(list(order_depthC.sell_orders.keys()))
+                        print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                        ordersC.append(Order(product, best_ask, LOT_SIZE))
+                        print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                        ordersC.append(Order(product, best_ask, LOT_SIZE))
+                        print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                        ordersC.append(Order(product, best_ask, LOT_SIZE))
+
+                    else:
+                        LOT_SIZE = int((600 - current_position) / 7)
+                        best_ask = 0.3 * (min(order_depthC.sell_orders.keys()) - TP) + TP
+                        print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                        ordersC.append(Order(product, best_ask, LOT_SIZE))
+                        print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                        ordersC.append(Order(product, best_ask, LOT_SIZE))
+                        print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                        ordersC.append(Order(product, best_ask, LOT_SIZE))
+                        print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                        ordersC.append(Order(product, best_ask, LOT_SIZE))
+
+                elif macd - macd_signal < 0:
+                    LOT_SIZE = int((600 + current_position) / 5)
+                    best_bid = max(TP - 0.2 * (TP - max(order_depthC.buy_orders.keys())),
+                                   max(order_depthC.buy_orders, key=order_depthC.buy_orders.get))
+                    print(f"{product} and {current_position} SELL at price {best_bid} with volume {LOT_SIZE}")
+                    ordersC.append(Order(product, best_bid, -LOT_SIZE))
+                    print(f"{product} and {current_position} SELL at price {best_bid} with volume {LOT_SIZE}")
+                    ordersC.append(Order(product, best_bid, -LOT_SIZE))
+                    print(f"{product} and {current_position} SELL at price {best_bid} with volume {LOT_SIZE}")
+                    ordersC.append(Order(product, best_bid, -LOT_SIZE))
+                    print(f"{product} and {current_position} SELL at price {best_bid} with volume {LOT_SIZE}")
+                    ordersC.append(Order(product, best_bid, -LOT_SIZE))
+
+                result[product] = ordersC
+
+            elif product == 'PINA_COLADAS':
+
+                ordersPi: list[Order] = []
+
+                order_depthPi: OrderDepth = state.order_depths[product]
+
+                if (len(position) == 0) or (product not in position.keys()):
+                    current_position = 0
+                else:
+                    current_position = position[product]
+
+                c1 = 12
+                c2 = 26
+                c3 = 9
+
+                m = int(state.timestamp / 100)
+
+                TP2 = float(min(order_depthPi.sell_orders.keys()) + max(order_depthPi.buy_orders.keys())) / 2
+                TP_list2.append(TP2)
+
+                if m > c2:
+                    SMAlong2 = np.mean(TP_list2[m - c2 - 1:m])
+
+                else:
+                    SMAlong2 = TP_list2[m - 1]
+                SMAlong_list2.append(SMAlong2)
+
+                print(SMAlong2)
+
+                if m > c1:
+                    SMAshort2 = np.mean(TP_list2[m - c1 - 1:m])
+
+                else:
+                    SMAshort2 = TP_list2[m - 1]
+                SMAshort_list2.append(SMAshort2)
+
+                print(SMAshort2)
+
+                k2 = 2 / (c2 + 1)
+                k1 = 2 / (c1 + 1)
+
+                EMAlong2 = k2 * SMAlong2 + (1 - k2) * EMAlong_list2[m - 1]
+                EMAlong_list2.append(EMAlong2)
+                print(EMAlong2)
+
+                EMAshort2 = k1 * SMAshort2 + (1 - k1) * EMAshort_list2[m - 1]
+                EMAshort_list2.append(EMAshort2)
+                print(EMAshort2)
+
+                macd2 = EMAshort2 - EMAlong2
+                macd_list2.append(macd2)
+
+                if m > c3:
+                    macd_signal2 = np.mean(macd_list2[m - c3 - 1:m])
+                else:
+                    macd_signal2 = macd_list2[m - 1]
+
+                print(macd2 - macd_signal2)
+
+                if macd2 - macd_signal2 > 0:
+                    LOT_SIZE = int((300 - current_position) / 6)
+                    best_ask = 0.2 * (min(order_depthPi.sell_orders.keys()) - TP2) + TP2
+                    print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                    ordersPi.append(Order(product, best_ask, LOT_SIZE))
+                    print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                    ordersPi.append(Order(product, best_ask, LOT_SIZE))
+                    print(f"{product} and {current_position} BUY at price {best_ask} with volume {LOT_SIZE}")
+                    ordersPi.append(Order(product, best_ask, LOT_SIZE))
+
+
+                elif macd2 - macd_signal2 < 0:
+                    LOT_SIZE = int((300 + current_position) / 6)
+                    best_bid = TP2 - 0.2 * (TP2 - max(order_depthPi.buy_orders.keys()))
+                    print(f"{product} and {current_position} SELL at price {best_bid} with volume {LOT_SIZE}")
+                    ordersPi.append(Order(product, best_bid, -LOT_SIZE))
+                    print(f"{product} and {current_position} SELL at price {best_bid} with volume {LOT_SIZE}")
+                    ordersPi.append(Order(product, best_bid, -LOT_SIZE))
+                    print(f"{product} and {current_position} SELL at price {best_bid} with volume {LOT_SIZE}")
+                    ordersPi.append(Order(product, best_bid, -LOT_SIZE))
+
+                result[product] = ordersPi
 
         return result
-
-import csv
-import os
-
-data = csv.reader(os.path.join("island-data-bottle-round-1", "island-data-bottle-round-1", "prices_round_1_day_0.csv"))
